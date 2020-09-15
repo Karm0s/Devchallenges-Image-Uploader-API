7e3895297eb036f0e4a65afb6f828ef2568958c5
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2020-09-15 17:18:34 +0100

    :sparkles: Delete images after 24 hours

--- routes/api/images.js
+++ routes/api/images.js
@@ -4,6 +4,8 @@ const path = require("path");
 const fs = require("fs");
 const crypto = require("crypto");
 const url = require("url");
+const { time } = require("console");
+const { basename } = require("path");
 
 const appDir = path.dirname(require.main.filename);
 
@@ -16,7 +18,12 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(
       null,
-      file.fieldname + crypto.randomBytes(10).toString('hex') + "-" + Date.now() + "-" + file.originalname.replace(' ', '-')
+      file.fieldname +
+        crypto.randomBytes(10).toString("hex") +
+        "-" +
+        Date.now() +
+        "-" +
+        file.originalname.replace(" ", "-")
     );
   },
 });
@@ -37,25 +44,40 @@ let upload = multer({
   },
 });
 
-router.post("/api/images", upload.single('image'), (req, res) => {
-  console.log(req.url);
+let timeOuts = []; // store timers to trigger image deletion
+let TIMEOUT = 1000 * 60 * 60 * 24; // 24 Hours timeout
+
+router.post("/api/images", upload.single("image"), (req, res) => {
+  console.log(req.file);
   if (req.fileValidationError) {
-    res.status(400).json({status: 400, error: 'file type not supported'});
+    res.status(400).json({ status: 400, error: "file type not supported" });
   } else {
-    res.json({status: 200, link: url.format({
-      protocol: req.protocol,
-      host: req.get('host'),
-      pathname: res.req.file.filename
-    })});
+    res.json({
+      status: 200,
+      link: url.format({
+        protocol: req.protocol,
+        host: req.get("host"),
+        pathname: res.req.file.filename,
+      }),
+    });
+    const timer = setTimeout(() => {
+      fs.unlink(path.join(appDir, req.file.path), (err) => {
+        if (err) throw err;
+      });
+    }, TIMEOUT);
+    timeOuts.push(timer);
   }
 });
 
 router.get("/:image", (req, res) => {
-  res.sendFile(path.join(appDir, process.env.IMAGE_STORAGE_LOCATION, req.params.image), (err) => {
-    if (err) {
-      res.status(404).send('Image not found');
+  res.sendFile(
+    path.join(appDir, process.env.IMAGE_STORAGE_LOCATION, req.params.image),
+    (err) => {
+      if (err) {
+        res.status(404).send("Image not found");
+      }
     }
-  });
+  );
 });
 
 module.exports = router;
